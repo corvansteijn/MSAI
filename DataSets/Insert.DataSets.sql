a3bdbd6fc92dc545282b0ef5092367a0b1aa9617
Cor van Steijn <cor@vansteijn.info>
2017-12-13 21:06:17 +0100

    fixup! initial version

--- DataSets/Insert.DataSets.sql
+++ DataSets/Insert.DataSets.sql
@@ -6,28 +6,28 @@ WITH cte AS (
 	union all 
 	select number + 1 from cte
 where number < 10000)
-INSERT INTO Images (Id, Data1, Data2, Result) 
+INSERT INTO TrainingSet(Id, Data1, Data2, Result) 
 	select 
 		number, 
 		ABS(CHECKSUM(NewId())) % 99, 
 		ABS(CHECKSUM(NewId())) % 99, 
 		0
 	from cte
-OPTION (MAXRECURSION 10000)
+OPTION (MAXRECURSION 10000);
 
 WITH cte AS (
 	select 1 as number
 	union all 
 	select number + 1 from cte
 where number < 200)
-INSERT INTO Images (Id, Data1, Data2, Result) 
+INSERT INTO TestSet(Id, Data1, Data2, Result) 
 	select 
 		number, 
 		ABS(CHECKSUM(NewId())) % 99, 
 		ABS(CHECKSUM(NewId())) % 99, 
 		0
 	from cte
-OPTION (MAXRECURSION 200)
+OPTION (MAXRECURSION 200);
 
 UPDATE TestSet SET Result = Data1 + Data2
 UPDATE TrainingSet SET Result = Data1 + Data2
